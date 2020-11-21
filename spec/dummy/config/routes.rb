edef80daeb7694bfa126ae4438c72a2ef35ca477
Georg Ledermann <georg@ledermann.dev>
2020-11-21 12:55:55 +0100

    Test inertia_share with redirects

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -15,10 +15,10 @@ Rails.application.routes.draw do
   put 'redirect_test' => 'inertia_test#redirect_test'
   delete 'redirect_test' => 'inertia_test#redirect_test'
   get 'my_location' => 'inertia_test#my_location'
-  get 'share_multithreaded' => 'inertia_multithreaded_share#share_multithreaded'
   get 'redirect_with_inertia_errors' => 'inertia_test#redirect_with_inertia_errors'
   post 'redirect_with_inertia_errors' => 'inertia_test#redirect_with_inertia_errors'
   get 'error_404' => 'inertia_test#error_404'
   get 'error_500' => 'inertia_test#error_500'
   get 'content_type_test' => 'inertia_test#content_type_test'
+  post 'redirect_to_share_test' => 'inertia_test#redirect_to_share_test'
 end
